2f38c335439393c4a03cdcdd1e2af4f412d477e5
Jakkapan Pandang <5735512156@psu.ac.th>
2020-03-19 13:12:37 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyA_RhJMgNpId_7gwNsXWHbbpmBSGhmpgB0",
+  authDomain: "todo-311156.firebaseapp.com",
+  databaseURL: "https://todo-311156.firebaseio.com",
+  projectId: "todo-311156",
+  storageBucket: "todo-311156.appspot.com",
+  messagingSenderId: "222866533050",
+  appId: "1:222866533050:web:1015428d7ac99c8c60f083",
+  measurementId: "G-Z8KQTVL510"
+};
+
+export default config
